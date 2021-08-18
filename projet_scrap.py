4e796b56dc5507262891f1c1753f4c75cb382299
michael <zerbib.michael@free.fr>
2021-08-18 17:16:24 +0200

    modification fichier projet_scrap.py ajout fonctionnalite creation csv

--- projet_scrap.py
+++ projet_scrap.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url='http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+urls='http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
 en_tete=['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available','product_description', 'category', 'review_rating', 'image_url']
 
-response= requests.get(url)
+response= requests.get(urls)
 
 soup=BeautifulSoup(response.content, features="html.parser")
 
@@ -16,59 +16,77 @@ soup=BeautifulSoup(response.content, features="html.parser")
 table=soup.find_all("td")
 
 #recuperer upc(code produit) dans la table
-upc=table[0].text
+upcs=table[0].text
 
 #recuperer titre
-titre=soup.find('h1')
-titre=titre.text
+titres=soup.find('h1')
+titres=titres.text
 
 #recuperer image url
 images=soup.find('img')
-lien_image=images["src"]
-lien_image=lien_image.replace('../..', 'https://books.toscrape.com')
+lien_images=images["src"]
+lien_images=lien_images.replace('../..', 'https://books.toscrape.com')
 
 
 #recuperer price_including_tax dans la table
-prix_TTC=table[3].text
+prix_TTCs=table[3].text
 
 
 #recuperer price_excluding_tax dans la table
-prix_HT=table[2].text
+prix_HTs=table[2].text
 
 #recuperer number_available dans la table
-stock=table[5].text
+stocks=table[5].text
 
 #recuperer product_description
 meta=soup.find_all("meta")
-description=meta[2]
-description=str(description)
-description=description.replace('<meta content="', ' ')
-description=description.replace('" name="description"/>', ' ')
+descriptions=meta[2]
+descriptions=str(descriptions)
+descriptions=descriptions.replace('<meta content="', ' ')
+descriptions=descriptions.replace('" name="description"/>', ' ')
+"""liste=[]
+liste.append(descriptions)
+descriptions=liste[0]"""
 
 #recuperer category
 lis=soup.findAll("li")
-category=lis[2].text
+categorys=lis[2].text
 	
 #recuperer review_rating dans la table
-review_rating=table[6].text	
+review_ratings=table[6].text	
 
+ligne=[]
+ligne.append(urls)
+ligne.append(upcs)
+ligne.append(titres)
+ligne.append(prix_TTCs)
+ligne.append(prix_HTs)
+ligne.append(stocks)
+ligne.append(descriptions)
+ligne.append(categorys)
+ligne.append(review_ratings)
+ligne.append(lien_images)
 
 with open('extraction_un_livre.csv', 'w') as csv_file:
 	writer=csv.writer(csv_file, delimiter=',')
 	writer.writerow(en_tete)
+	writer.writerow(ligne)
 
 
+	"""for url,upc,titre,prix_TTC,prix_HT,stock,description,category,review_rating,lien_image in zip(urls,upcs,titres,prix_TTCs,prix_HTs,stocks,descriptions,categorys,review_ratings,lien_images):
+		writer.writerow([url,upc,titre,prix_TTC,prix_HT,stock,description,category,review_rating,lien_image])"""
 
-print(url)
-print(upc)
-print(titre)
-print(prix_TTC)
-print(prix_HT)
-print(stock)
-print(description)
-print(category)
-print(review_rating)
-print(lien_image)
+
+print(urls)
+print(upcs)
+print(titres)
+print(prix_TTCs)
+print(prix_HTs)
+print(stocks)
+print(descriptions)
+print(categorys)
+print(review_ratings)
+print(lien_images)
 
 
 
